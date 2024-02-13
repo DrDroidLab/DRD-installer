35e5cbde58d1b1d9de1c3242b2b5cdd6a5a84a45
Dipesh Mittal <dipesh@drdroid.io>
2024-02-13 19:18:37 +0530

    Update import_alert_metrics.py
    Modified for grafana 8.3.4

--- import_alert_metrics.py
+++ import_alert_metrics.py
@@ -10,9 +10,6 @@ import os
 GRAFANA_URL = os.getenv('GRAFANA_HOST')
 GRAFANA_API_KEY = os.getenv('GRAFANA_API_KEY')
 
-# Prometheus API parameters
-PROMETHEUS_QUERY_RANGE_URL = os.getenv('PROMETHEUS_HOST') + "/api/v1/query_range"
-
 
 # Function to fetch alerts from Grafana
 def fetch_alerts():
@@ -21,7 +18,7 @@ def fetch_alerts():
         "Content-Type": "application/json"
     }
 
-    response = requests.get(f"{GRAFANA_URL}/api/v1/provisioning/alert-rules", headers=headers)
+    response = requests.get(f"{GRAFANA_URL}/api/ruler/grafana/api/v1/rules", headers=headers)
     alerts = response.json()
     return alerts
 
@@ -46,11 +43,9 @@ def query_prometheus_from_grafana(ds_id, metric_query):
         "Content-Type": "application/json"
     }
 
-    url = GRAFANA_URL + '/api/datasources/proxy/uid/{}/api/v1/query_range'.format(ds_id) + '?query={}&start={}&end={}&step={}'.format(metric_query, datetime.strftime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ'), datetime.strftime(end_time, '%Y-%m-%dT%H:%M:%S.%fZ'), "1m")
+    url = GRAFANA_URL + '/api/datasources/proxy/{}/api/v1/query_range'.format(ds_id) + '?query={}&start={}&end={}&step={}'.format(metric_query, datetime.strftime(start_time, '%Y-%m-%dT%H:%M:%S.%fZ'), datetime.strftime(end_time, '%Y-%m-%dT%H:%M:%S.%fZ'), "1m")
 
-    print('url: ', url)
     response = requests.get(url, headers=headers)
-    print('response: ', response.text)
     data = response.json()
     return data
 
@@ -61,44 +56,47 @@ def main():
     # fetch datasources
     datasources = fetch_datasources()
     prometheus_datasources = []
+    prometheus_datasources_dict = {}
     for ds in datasources:
         if ds['type'] == 'prometheus':
             prometheus_datasources.append(ds['uid'])
+            prometheus_datasources_dict[ds['uid']] = ds['id']
 
     data_source_metrics_exprs_dict = {}
 
     wb = Workbook()
     alerts_ws = wb.active
     alerts_ws.title = "alerts"
-    alerts_ws['A1'] = 'UID'
-    alerts_ws['B1'] = 'Title'
-    alerts_ws['C1'] = 'JSON'
+    alerts_ws['A1'] = 'Title'
+    alerts_ws['B1'] = 'JSON'
 
-    alert_row_index = 1
+    alert_row_index = 2
     # Fetch alerts from Grafana
     alerts = fetch_alerts()
     # Process each alert
-    for alert in alerts:
-        alerts_ws[f"A{alert_row_index}"] = alert['uid']
-        alerts_ws[f"B{alert_row_index}"] = alert['title']
-        alerts_ws[f"C{alert_row_index}"] = json.dumps(alert)
 
-        for data in alert['data']:
-            if data.get('datasourceUid') and data.get('datasourceUid') in prometheus_datasources:
-                if data.get('datasourceUid') not in data_source_metrics_exprs_dict:
-                    data_source_metrics_exprs_dict[data.get('datasourceUid')] = []
-                if data['model'].get('expr'):
-                    data_source_metrics_exprs_dict[data.get('datasourceUid')].append(data['model'].get('expr'))
+    for f in alerts.keys():
+        for alert in alerts[f]:
+            alerts_ws[f"A{alert_row_index}"] = alert['name']
+            alerts_ws[f"B{alert_row_index}"] = json.dumps(alert)
+
+            for rule in alert['rules']:
+                for data in rule['grafana_alert']['data']:
+                    if data.get('datasourceUid') and data.get('datasourceUid') in prometheus_datasources:
+                        if data.get('datasourceUid') not in data_source_metrics_exprs_dict:
+                            data_source_metrics_exprs_dict[data.get('datasourceUid')] = []
+                        if data['model'].get('expr'):
+                            data_source_metrics_exprs_dict[data.get('datasourceUid')].append(data['model'].get('expr'))
 
     metrics_ws = wb.create_sheet(title="metrics")
     metrics_ws['A1'] = 'Metric Query'
     metrics_ws['B1'] = 'Metric Values'
 
-    metric_row_index = 1
+    metric_row_index = 2
 
     for ds_id in prometheus_datasources[:1]:
         for metric_query in data_source_metrics_exprs_dict[ds_id][:1]:
-            metric_values = query_prometheus_from_grafana(ds_id, metric_query)
+            metric_values = query_prometheus_from_grafana(prometheus_datasources_dict[ds_id], metric_query)
             metrics_ws[f"A{metric_row_index}"] = metric_query
             metrics_ws[f"B{metric_row_index}"] = json.dumps(metric_values)
 
